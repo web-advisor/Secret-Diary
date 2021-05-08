22968fe7ad6fabe934368a1a0d8f16d2240b6844
web-advisor <75082566+web-advisor@users.noreply.github.com>
2021-05-08 18:31:30 +0530

    Updating Database

---
+++ updateDatabase.php
@@ -0,0 +1,12 @@
+<?php
+    session_start();
+        if(isset($_POST['content'])){
+        include("databaseConnecting.php");
+        $query="UPDATE `users` SET `diary`='".mysqli_real_escape_string($link,$_POST['content'])."' WHERE `id`='".mysqli_real_escape_string($link,$_SESSION['id'])."' LIMIT 1";
+        mysqli_query($link,$query);
+        #echo $_POST['content'];
+    }
+
+
+
+?>
