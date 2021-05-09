f80aedfe7ce96d5153e43c6addacd4e647957a0a
web-advisor <sinnaamar68@gmail.com>
2021-05-09 17:37:51 +0530

    Another Chance

--- updateDatabase.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-        if(isset($_POST['content'])){
-        include("databaseConnecting.php");
-        $query="UPDATE `users` SET `diary`='".mysqli_real_escape_string($link,$_POST['content'])."' WHERE `id`='".mysqli_real_escape_string($link,$_SESSION['id'])."' LIMIT 1";
-        mysqli_query($link,$query);
-        #echo $_POST['content'];
-    }
-?>
