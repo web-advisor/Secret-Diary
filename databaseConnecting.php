a80f7e7ea004369c302db57d6ca815e9bdace5e0
web-advisor <sinnaamar68@gmail.com>
2021-05-10 12:14:11 +0530

    New DB

--- databaseConnecting.php
+++ databaseConnecting.php
@@ -5,11 +5,17 @@
         // $password="webadvisor@0401";
         // $dbName="secret_diary";
 
-    # Production Database Info 
-        $host="remotemysql.com";
-        $userName="otHXxPN3zw";
-        $password="Zky8vdvuB0";
-        $dbName="otHXxPN3zw";
+    # Remote Database Info 1
+        // $host="remotemysql.com";
+        // $userName="otHXxPN3zw";
+        // $password="Zky8vdvuB0";
+        // $dbName="otHXxPN3zw";
+
+    # Remote Database Info 1
+        $host="db4free.net";
+        $userName="attaboy_dev";
+        $password="attaboydev";
+        $dbName="secret_diary";
 
         $link=mysqli_connect($host,$userName,$password,$dbName);        
         if(mysqli_connect_error()){
